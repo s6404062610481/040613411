1d1e93fc3b08088915e11c60e87d9052463317cd
s6404062610481 <138811422+s6404062610481@users.noreply.github.com>
2023-07-11 14:57:29 +0700

    project hw0

---
+++ hw0/js/script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween:20,
+    centeredSlides: false,
+    slidesPerGroupSkip: 3,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
---
+++ hw0/js/script2.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide-content-2", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+  });
