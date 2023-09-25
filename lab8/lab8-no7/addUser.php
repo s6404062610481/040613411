5f02fa132bce0c0dbedc112cf25820d3f089526c
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-09-26 00:25:36 +0700

    Create addUser.php

---
+++ lab8/lab8-no7/addUser.php
@@ -0,0 +1,25 @@
+<?php 
+    include "conection.php"
+?> 
+
+<?php
+
+    $stmt = $pdo->prepare("INSERT INTO member
+    
+    VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1, $_POST["username"]);
+    $stmt->bindParam(2, $_POST["password"]);
+    $stmt->bindParam(3, $_POST["name"]);
+    $stmt->bindParam(4, $_POST["address"]);
+    $stmt->bindParam(5, $_POST["mobile"]);
+    $stmt->bindParam(6, $_POST["email"]);
+    $stmt->bindParam(7, $_POST["img"]);
+    $stmt->execute(); // เริ่มเพิ่มข้อมูล // ขอคีย์หลักที่เพิ่มส าเร็จ
+    
+    ?>
+    <html>
+    <head><meta charset="UTF-8"></head>
+    <body>
+    เพิ่มสมาชิกสำเร็จ
+    </body>
+</html>
