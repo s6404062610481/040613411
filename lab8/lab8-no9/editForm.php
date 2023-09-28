0b2f25a0b430d63b277419a42e4226525eeed6c2
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-09-28 17:08:30 +0700

    Update editForm.php

--- lab8/lab8-no9/editForm.php
+++ lab8/lab8-no9/editForm.php
@@ -1,7 +1,7 @@
 <?php include "conection.php" ?>
 <?php
 
-$stmt = $pdo->prepare("SELECT * FROM member WHERE username = ?");
+$stmt = $pdo->prepare("SELECT username, name, address, email, img FROM member WHERE username = ?");
 $stmt->bindParam(1, $_GET["username"]);
 $stmt->execute();
 $row = $stmt->fetch();
@@ -9,12 +9,14 @@ $row = $stmt->fetch();
 <html>
 <head><meta charset="utf-8"></head>
 <body>
-<form action="editDataForm.php" method="post">
-    <input type="hidden" name="username" value="<?=$row["username"]?>">
-    ชื่อสมาชิก : <input type="text" name="name" value="<?=$row["name"]?>"><br>
-    ที่อยู่ : <input type="text" name="address" value="<?=$row["address"]?>"><br>
-    อีเมลล์: <input type="text" name="email" value="<?=$row["email"]?>"><br>
-    <input type="submit" value="แก้ไขข้อมูล ">
+<form action="editDataForm.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="username" value="<?=$row["username"]?>"><br><br>
+    Name: <input type="text" name="name" value="<?=$row["name"]?>"><br><br>
+    Address: <input type="text" name="address" value="<?=$row["address"]?>"><br><br>
+    Email: <input type="text" name="email" value="<?=$row["email"]?>"><br><br>
+    Select Image File to Update:
+    <input type="file" name="file"><br>
+    <input type="submit" name="submit" value="Update">
 </form>
 </body>
 </html>
